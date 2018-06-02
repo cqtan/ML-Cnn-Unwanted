cf2c9fa2e72678cc9163a007331dc3b8b9aa4b68
CouchCat <s0547139@htw-berlin.de>
2018-06-03 01:44:06 +0200

    Replace cv2 with PIL alternatives.

--- classify-all.py
+++ classify-all.py
@@ -4,21 +4,17 @@
 # import the necessary packages
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
-import matplotlib.pyplot as plt
 from imutils import paths
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 import argparse
 import pickle
-from skimage.transform import resize
-from skimage.io import imread
-
-import cv2
+from PIL import Image
 import os
 
-# Configure amount of GPU memory in % to allocate:
+""" # Configure amount of GPU memory in % to allocate:
 config = tf.GPUOptions(per_process_gpu_memory_fraction=0.870)
-sess = tf.Session(config=tf.ConfigProto(gpu_options=config))
+sess = tf.Session(config=tf.ConfigProto(gpu_options=config)) """
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -43,25 +39,22 @@ if not os.path.exists('output'):
 		os.makedirs('output/'+cl)
 
 # loop over all images in each subdirectory
+print("[INFO] classifying images...")
 imagePaths = sorted(list(paths.list_images(args["images"])))
 for imagePath in imagePaths:
 	# load and save a copy of the image
-	image = cv2.imread(imagePath)
-	#image = plt.imread(imagePath)
-	#image = imread(imagePath, plugin='tifffile')
+	image = Image.open(imagePath)
 	output = image.copy()
 	ppn = imagePath.split(os.path.sep)[-2]
 	filename = imagePath.split(os.path.sep)[-1]
 
 	# pre-process the image for classification
-	image = cv2.resize(image, (96, 96))
-	#image = resize(image, (96, 96))
-	image = image.astype("float") / 255.0
-	image = img_to_array(image)
+	image = image.resize((96, 96))
+	image = np.array(image)
+	image = np.divide(image, 255.0)
 	image = np.expand_dims(image, axis=0)
 
 	# classify the input image
-	print("[INFO] classifying image...")
 	proba = model.predict(image)[0]
 	idx = np.argmax(proba)
 	label = lb.classes_[idx]
@@ -71,5 +64,6 @@ for imagePath in imagePaths:
 	out_path = 'output/'+label+'/'+ppn
 	if not os.path.exists(out_path):
 		os.makedirs(out_path)
-	#cv2.imwrite(os.path.join(out_path,filename), output)
-	plt.imsave(os.path.join(out_path,filename), output)
+	output.save(os.path.join(out_path,filename))
+
+print("[INFO] Done! Output directory created!")
